cb89ca87ef26df5f7267758fd628a270908cf652
Andrew Jesson <andrew.jesson@cs.ox.ac.uk>
2021-09-10 00:21:03 +0100

    Add interfaces

--- clouds/main.py
+++ clouds/main.py
@@ -42,7 +42,84 @@ def train(
             "job_dir": job_dir,
             "gpu_per_trial": gpu_per_model,
             "seed": seed,
-            "tune": False,
+            "mode": "train",
+        }
+    )
+
+
+@cli.command("tune")
+@click.option(
+    "--job-dir",
+    type=str,
+    required=True,
+    help="location for writing checkpoints and results",
+)
+@click.option(
+    "--max-samples",
+    default=200,
+    type=int,
+    help="maximum number of search space samples, default=100",
+)
+@click.option(
+    "--gpu-per-model",
+    default=cuda.device_count(),
+    type=float,
+    help="number of gpus for each ensemble model, default=cuda.device_count()",
+)
+@click.option(
+    "--seed", default=1331, type=int, help="random number generator seed, default=1331",
+)
+@click.pass_context
+def tune(
+    context, job_dir, max_samples, gpu_per_model, seed,
+):
+    ray.init(
+        num_gpus=context.obj["n_gpu"],
+        dashboard_host="127.0.0.1",
+        ignore_reinit_error=True,
+    )
+    context.obj.update(
+        {
+            "job_dir": job_dir,
+            "max_samples": max_samples,
+            "gpu_per_trial": gpu_per_model,
+            "seed": seed,
+            "mode": "tune",
+        }
+    )
+
+
+@cli.command("predict")
+@click.option(
+    "--job-dir",
+    type=str,
+    required=True,
+    help="location for writing checkpoints and results",
+)
+@click.option(
+    "--gpu-per-model",
+    default=cuda.device_count(),
+    type=float,
+    help="number of gpus for each ensemble model, default=cuda.device_count()",
+)
+@click.option(
+    "--seed", default=1331, type=int, help="random number generator seed, default=1331",
+)
+@click.pass_context
+def predict(
+    context, job_dir, gpu_per_model, seed,
+):
+    ray.init(
+        num_gpus=context.obj["n_gpu"],
+        dashboard_host="127.0.0.1",
+        ignore_reinit_error=True,
+    )
+    context.obj.update(
+        {
+            "job_dir": job_dir,
+            "gpu_per_trial": gpu_per_model,
+            "seed": seed,
+            "mode": "predict",
         }
     )
 
@@ -67,6 +144,117 @@ def jasmin(
             "ds_test": {"root": root, "split": "test",},
         }
     )
+
+
+@cli.command("ensemble")
+@click.pass_context
+@click.option("--dim-hidden", default=400, type=int, help="num neurons")
+@click.option("--num-components", default=5, type=int, help="num mixture components")
+@click.option("--depth", default=3, type=int, help="depth of feature extractor")
+@click.option(
+    "--negative-slope",
+    default=-1,
+    type=float,
+    help="negative slope of leaky relu, default=-1 use elu",
+)
+@click.option(
+    "--dropout-rate", default=0.15, type=float, help="dropout rate, default=0.1"
+)
+@click.option(
+    "--spectral-norm",
+    default=0.95,
+    type=float,
+    help="Spectral normalization coefficient. If 0.0 do not use spectral norm, default=0.0",
+)
+@click.option(
+    "--learning-rate",
+    default=1e-3,
+    type=float,
+    help="learning rate for gradient descent, default=1e-3",
+)
+@click.option(
+    "--batch-size",
+    default=32,
+    type=int,
+    help="number of examples to read during each training step, default=100",
+)
+@click.option(
+    "--epochs", type=int, default=500, help="number of training epochs, default=50"
+)
+@click.option(
+    "--ensemble-size",
+    type=int,
+    default=10,
+    help="number of models in ensemble, default=1",
+)
+def ensemble(
+    context,
+    dim_hidden,
+    num_components,
+    depth,
+    negative_slope,
+    dropout_rate,
+    spectral_norm,
+    learning_rate,
+    batch_size,
+    epochs,
+    ensemble_size,
+):
+    if context.obj["mode"] == "tune":
+        context.obj.update(
+            {"epochs": epochs, "ensemble_size": ensemble_size,}
+        )
+        print("tuning")
+    else:
+        context.obj.update(
+            {
+                "dim_hidden": dim_hidden,
+                "depth": depth,
+                "num_components": num_components,
+                "negative_slope": negative_slope,
+                "dropout_rate": dropout_rate,
+                "spectral_norm": spectral_norm,
+                "learning_rate": learning_rate,
+                "batch_size": batch_size,
+                "epochs": epochs,
+                "ensemble_size": ensemble_size,
+            }
+        )
+
+        if context.obj["mode"] == "train":
+
+            @ray.remote(num_gpus=context.obj.get("gpu_per_trial"),)
+            def trainer(**kwargs):
+                func = lambda args: print("training")
+                return func
+
+            results = []
+            for ensemble_id in range(ensemble_size):
+                results.append(
+                    trainer.remote(
+                        config=context.obj,
+                        experiment_dir=context.obj.get("experiment_dir"),
+                        ensemble_id=ensemble_id,
+                    )
+                )
+            ray.get(results)
+        elif context.obj["mode"] == "predict":
+
+            @ray.remote(num_gpus=context.obj.get("gpu_per_trial"),)
+            def predictor(**kwargs):
+                func = lambda args: print("predicting")
+                return func
+
+            results = []
+            for ensemble_id in range(ensemble_size):
+                results.append(
+                    predictor.remote(
+                        config=context.obj,
+                        experiment_dir=context.obj.get("experiment_dir"),
+                        ensemble_id=ensemble_id,
+                    )
+                )
+            ray.get(results)
 
 
 if __name__ == "__main__":
