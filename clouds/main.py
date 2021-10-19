2a12a4960b1da38f5ed80ca51a416b731b3a7acf
Andrew Jesson <andrew.jesson@cs.ox.ac.uk>
2021-10-19 13:06:29 +0100

    add attention interface in main

--- clouds/main.py
+++ clouds/main.py
@@ -148,6 +148,28 @@ def jasmin(
     )
 
 
+@cli.command("jasmin-daily")
+@click.pass_context
+@click.option(
+    "--root", type=str, required=True, help="location of dataset",
+)
+def jasmin_daily(
+    context, root,
+):
+    job_dir = Path(context.obj.get("job_dir"))
+    dataset_name = "jasmin-daily"
+    experiment_dir = job_dir / dataset_name
+    context.obj.update(
+        {
+            "dataset_name": dataset_name,
+            "experiment_dir": str(experiment_dir),
+            "ds_train": {"root": root, "split": "train",},
+            "ds_valid": {"root": root, "split": "valid",},
+            "ds_test": {"root": root, "split": "test",},
+        }
+    )
+
+
 @cli.command("ensemble")
 @click.pass_context
 @click.option("--dim-hidden", default=800, type=int, help="num neurons")
@@ -202,11 +224,14 @@ def ensemble(
     epochs,
     ensemble_size,
 ):
+    context.obj.update(
+        {"experiment_dir": str(Path(context.obj["experiment_dir"]) / "ensemble")}
+    )
     if context.obj["mode"] == "tune":
         context.obj.update(
             {"epochs": epochs, "ensemble_size": ensemble_size,}
         )
-        workflows.tuning.hyper_tune(config=context.obj)
+        workflows.tuning.neural_network.run(config=context.obj)
     else:
         context.obj.update(
             {
@@ -227,7 +252,7 @@ def ensemble(
 
             @ray.remote(num_gpus=context.obj.get("gpu_per_model"),)
             def trainer(**kwargs):
-                func = workflows.training.train_ensemble(**kwargs)
+                func = workflows.training.neural_network.train(**kwargs)
                 return func
 
             results = []
@@ -242,7 +267,112 @@ def ensemble(
             ray.get(results)
         elif context.obj["mode"] == "predict":
 
-            workflows.prediction.predict_ensemble(
+            workflows.prediction.neural_network.predict(
+                config=context.obj, experiment_dir=context.obj.get("experiment_dir")
+            )
+
+
+@cli.command("attention")
+@click.pass_context
+@click.option("--dim-hidden", default=200, type=int, help="num neurons")
+@click.option("--num-components", default=10, type=int, help="num mixture components")
+@click.option("--depth", default=2, type=int, help="depth of feature extractor")
+@click.option("--num-heads", default=2, type=int, help="multi-head attention parameter")
+@click.option(
+    "--negative-slope",
+    default=0.2,
+    type=float,
+    help="negative slope of leaky relu, default=-1 use elu",
+)
+@click.option(
+    "--spectral-norm",
+    default=3.0,
+    type=float,
+    help="Spectral normalization coefficient. If 0.0 do not use spectral norm, default=0.0",
+)
+@click.option(
+    "--dropout-rate", default=0.1, type=float, help="dropout rate, default=0.1"
+)
+@click.option(
+    "--learning-rate",
+    default=1e-3,
+    type=float,
+    help="learning rate for gradient descent, default=1e-3",
+)
+@click.option(
+    "--batch-size",
+    default=64,
+    type=int,
+    help="number of examples to read during each training step, default=100",
+)
+@click.option(
+    "--epochs", type=int, default=200, help="number of training epochs, default=400"
+)
+@click.option(
+    "--ensemble-size",
+    type=int,
+    default=10,
+    help="number of models in ensemble, default=1",
+)
+def attention(
+    context,
+    dim_hidden,
+    num_components,
+    depth,
+    num_heads,
+    negative_slope,
+    spectral_norm,
+    dropout_rate,
+    learning_rate,
+    batch_size,
+    epochs,
+    ensemble_size,
+):
+    context.obj.update(
+        {"experiment_dir": str(Path(context.obj["experiment_dir"]) / "attention")}
+    )
+    if context.obj["mode"] == "tune":
+        context.obj.update(
+            {"epochs": epochs, "ensemble_size": ensemble_size,}
+        )
+        workflows.tuning.attention.run(config=context.obj)
+    else:
+        context.obj.update(
+            {
+                "dim_hidden": dim_hidden,
+                "depth": depth,
+                "num_heads": num_heads,
+                "num_components": num_components,
+                "negative_slope": negative_slope,
+                "dropout_rate": dropout_rate,
+                "spectral_norm": spectral_norm,
+                "learning_rate": learning_rate,
+                "batch_size": batch_size,
+                "epochs": epochs,
+                "ensemble_size": ensemble_size,
+            }
+        )
+
+        if context.obj["mode"] == "train":
+
+            @ray.remote(num_gpus=context.obj.get("gpu_per_model"),)
+            def trainer(**kwargs):
+                func = workflows.training.attention.train(**kwargs)
+                return func
+
+            results = []
+            for ensemble_id in range(ensemble_size):
+                results.append(
+                    trainer.remote(
+                        config=context.obj,
+                        experiment_dir=context.obj.get("experiment_dir"),
+                        ensemble_id=ensemble_id,
+                    )
+                )
+            ray.get(results)
+        elif context.obj["mode"] == "predict":
+
+            workflows.prediction.attention.predict(
                 config=context.obj, experiment_dir=context.obj.get("experiment_dir")
             )
 
